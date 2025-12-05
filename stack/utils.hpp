97abedd8087de7d32dbaa43e15d894eead2afec0
Karl <karlvincent.illusorio@cit.edu>
2025-12-05 18:21:06 +0800

    Added my implementation

--- stack/utils.hpp
+++ stack/utils.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include <string>
 
@@ -13,7 +12,32 @@ namespace adt {
             Stack* stack = new StackSll();
             int result;
 
-            //Implement solution/code here
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                    stack->push(s[i]);
+                } else {
+                    if ((stack->top() == '(' && s[i] == ')') || (stack->top() == '{' && s[i] == '}')
+                    || (stack->top() == '[' && s[i] == ']')) {
+                        stack->pop();
+                    } else if ((stack->isEmpty() && s[i] == ')') || (stack->isEmpty() && s[i] == '}')
+                    || (stack->isEmpty() && s[i] == ']')) {
+                        result = 3;
+                        delete stack;
+                        return result;
+                    } else if ((stack->top() == '(' && s[i] != ')') || (stack->top() == '{' && s[i] != '}')
+                    || (stack->top() == '[' && s[i] != ']')) {
+                        result = 1;
+                        delete stack;
+                        return result;
+                    }
+                }
+            }
+
+            if (stack->isEmpty()) {
+                result = 0;
+            } else {
+                result = 2;
+            }
 
             delete stack;
             return result;
